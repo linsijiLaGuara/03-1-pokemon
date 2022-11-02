57d00e015582b8375497a1ddb73ee6e1bffef7fd
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-02 14:36:54 -0300

    axion es prueba !

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
 
 
 @Injectable()
 export class SeedService {
- executeSeed(){
-  console.log(fetch);
-  return 'Seed executed'
- }
+
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed(){
+    
+    const {data} = await this.axios.get('https://pokeapi.co/api/v2/pokemon?limit=650');
+    return data;
+  }
 }
