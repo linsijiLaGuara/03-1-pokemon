3c7465046ccd2e8509980dd64a1eb1b6e52db914
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-03 17:26:09 -0300

    add: validacio en los mudlo

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,5 +5,5 @@ export const EnvConfiguration= ()=>({
 envarioment: process.env.NODE_ENV ||'dev',
 mongodb: process.env.MONGODB,
 port: process.env.PORT||3002,
-defaultlimit: process.env.DEFAULT_LIMIT||6
+defaultlimit: process.env.DEFAULT_LIMIT||5
 })
