2ea86485a0947a318ae5a36391c30cb6e6f10058
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-03 10:00:27 -0300

    add: codigo pata insertar

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { async } from 'rxjs';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
@@ -19,13 +20,15 @@ constructor(
  async executeSeed(){
    const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
     
-    data.results.forEach(({name, url})=> {
+    data.results.forEach(async({name, url})=> {
 
     const segments = url.split('/');
     const no = +segments [segments.length - 2];
-    console.log({name, no})
+ //inserccion lo busque en pokeomon services para insertar
+    const pokemon = await this.pokemonModel.create( {name, no} );
+
     })
 
-    return data.results;
+    return 'seed ejecutado';
   }
 }
