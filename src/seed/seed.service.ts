293551f7396d87342355998e2de6b0e2c8159e5e
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-03 11:05:36 -0300

    add: para multiples registro

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { throws } from 'assert';
 import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { async } from 'rxjs';
@@ -21,10 +22,11 @@ constructor(
   await this.pokemonModel.deleteMany({});//delete * from pokemon
 
 
-   const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
-    //insertar promesas 
-const inserPromisesArray = [];
-
+   const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    //insertar promesas  multiples insercciones
+  //const inserPromisesArray = [];
+  //arreglo para insertar 
+  const pokemonToInsert:{name: string, no : number }[] = [];
 
     data.results.forEach(({name, url})=> {
 
@@ -33,14 +35,15 @@ const inserPromisesArray = [];
  //inserccion lo busque en pokeomon services para insertar
    // const pokemon = await this.pokemonModel.create( {name, no} );
      
-   inserPromisesArray.push( 
-      this.pokemonModel.create( {name, no} )
-     )
+   pokemonToInsert.push( {name, no} ); //arreglo
     });
 
     //espera que todas la promeses se inserten hace todas las insersiones de manera sinmultanea
-   await Promise.all(inserPromisesArray);
-
+  // await Promise.all(inserPromisesArray);
+  
+  
+  //inserta el arreglo con muchas entradas
+ await this.pokemonModel.insertMany(pokemonToInsert);
     return 'seed ejecutado';
   }
 }
