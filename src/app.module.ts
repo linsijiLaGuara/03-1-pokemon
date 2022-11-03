2e363732e6b8a38f07c4948e3d97d0faa7e9a150
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-03 13:08:20 -0300

    ADD: variable de enterno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -7,21 +8,29 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
+
 @Module({
   imports: [
-    
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), 
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
     CommonModule,
 
     SeedModule,
+    
 
   ],
 })
-export class AppModule {}
+export class AppModule {
+constructor(){
+  console.log(process.env)
+}
+
+
+}
